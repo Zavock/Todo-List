ec0990dd5f49d6a78684188da9e71b5a6fa0b599
Juan Ospina <zuleta2411@hotmail.com>
2023-08-10 13:58:23 -0500

    TodoForm, realizado

---
+++ src/TodoForm/index.js
@@ -0,0 +1,48 @@
+import React, {useContext, useState} from 'react';
+import './TodoForm.css'
+import CampFire from '../CampFire';
+import { TodoContext } from '../TodoContext';
+
+const TodoForm = () => {
+  const [newValueTodo, setNewValueTodo] = useState('');
+  const {
+    addTodo,
+    setOpenModal
+  } = useContext(TodoContext);
+
+  const styleCampfire = {
+    position:'absolute',
+    bottom: '28%',
+    left: '36.7%',
+    height: '100px',
+    width: '100px',
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    addTodo(newValueTodo)
+    setOpenModal(false)
+  }
+
+  const onCancel = () => {
+    setOpenModal(false)
+  }
+
+  const onChange = (e) => {
+    setNewValueTodo(e.target.value);
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <label>Ingresa la actividad</label>
+      <textarea onChange={onChange}></textarea>
+      <div className="TodoForm-buttonContainer">
+        <button type='' className="TodoForm-button TodoForm-button--cancel" onClick={onCancel}>Cancelar</button>
+        <button type='submit' className="TodoForm-button TodoForm-button--add">Agregar</button>
+      </div>
+      <CampFire style={styleCampfire} />
+    </form>
+  );
+}
+
+export  {TodoForm};
