d47dcf8698af02f94fa94b7cacb45847b0efecc2
Juan Ospina <zuleta2411@hotmail.com>
2023-08-10 14:05:21 -0500

    TodoList, realizado

---
+++ src/TodoList/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TodoList.css'
+
+const TodoList = (props) => {
+  return (
+    <ul className='TodoList'>
+      {props.children}
+    </ul>
+  );
+}
+
+export { TodoList };
