3e564f2b506ae7bbb1f138a6f94fb6543c67a3d7
Juan Ospina <zuleta2411@hotmail.com>
2023-08-10 14:06:41 -0500

    TodoSearch, realizado

---
+++ src/TodoSearch/index.js
@@ -0,0 +1,17 @@
+import React,{useContext} from 'react';
+import './TodoSearch.css'
+import { TodoContext } from '../TodoContext';
+
+const TodoSearch = () => {
+  const {
+    searchValue,
+    setSearchValue
+  } = useContext(TodoContext);
+  return (
+    <input className='TodoSearch' placeholder='Buscar' value={searchValue} onChange={(e) => {
+      setSearchValue(e.target.value);
+    }} />
+  );
+}
+
+export { TodoSearch };
